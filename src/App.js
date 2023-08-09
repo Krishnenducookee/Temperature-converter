be6177da8647f5ccb73a5414a7d2b31887fca6cb
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-09 11:32:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import { useState } from "react";
 import Page from "./Page";
 
 function App({ label, onChange, value, name }) {
-  const [Temperature, setTemperature] = useState(0);
-  // const [farenheit, setfarenheit] = useState(32);
+  const [Temperature, setTemperature] = useState({
+    Celsius: 0,
+    Farenheit: 32,
+  });
   const labels = [
-    { name: "NewCelsius", label: "Celsius" },
-    { name: "NewFarenheit", label: "Farenheit" },
+    { name: "Celsius", label: "Celsius" },
+    { name: "Farenheit", label: "Farenheit" },
   ];
-  const changeTemerature = (temperature,name) => {
-    console.log(temperature);
-    if (name == "NewCelsius") {
-      // setTemperature(temperature);
-      setTemperature((temperature * 9) / 5 + 32);
-      console.log(temperature);
+  const changeTemerature = (e) => {
+    const name = e.taret.name;
+    const value = e.target.value;
+    setTemperature({ ...Temperature, [name]: value });
+    if (name) {
+      setTemperature((Temperature * 9) / 5 + 32);
     } else setTemperature((Temperature * 9) / 5 + 32);
   };
-
   // const changecelsius = (Farenheit) => {
   //   console.log(Farenheit);
   //   setfarenheit(Farenheit);
@@ -29,27 +30,15 @@ function App({ label, onChange, value, name }) {
       Temperature
       <div className="flex flex-row mx-96 mt-10 h-40 border-2 border-purple-800 p-5">
         <div>
-          {labels.map((data) => (
+          {labels.map((data, key) => (
             <Page
               label={data.label}
               value={Temperature}
               name={data.name}
-              onChange={(e) => {
-                changeTemerature(e.target.value,e.target.name);
-              }}
+              onChange={changeTemerature}
             />
           ))}
         </div>
-        {/* <div className="pl-5 space-y-3">
-          <Page
-            label="faren"
-            // name={data.name}
-            value={farenheit}
-            onChange={(e) => {
-              changecelsius(e.target.value);
-            }}
-          />
-        </div> */}
       </div>
       <div className="mx-96 mt-10">
         {Temperature} <sup>o</sup> C = {Temperature}
