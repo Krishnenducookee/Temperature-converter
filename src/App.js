c92711f36d004d4c9be3d4758c559b4879cf791d
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-09 13:04:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import { useState } from "react";
 import Page from "./Page";
 
-function App({ label, onChange, value, name }) {
-  const [Temperature, setTemperature] = useState({
-    Celsius: 0,
-    Farenheit: 32,
-  });
+function App() {
+  const [Temperature, setTemperature] = useState({});
   const labels = [
-    { name: "Celsius", label: "Celsius" },
-    { name: "Farenheit", label: "Farenheit" },
+    {
+      label: "Celsius",
+      onChange: (newvalue) => {
+        changeTemerature({ newvalue, iscelsius: true });
+      },
+    },
+    {
+      label: "Farenheit",
+      onChange: (newvalue) => {
+        changeTemerature({ newvalue, iscelsius: false });
+      },
+    },
   ];
-  const changeTemerature = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setTemperature({ ...Temperature, [name]: value });
-    if (name) {
-      setTemperature((Temperature * 9) / 5 + 32);
-    } else setTemperature((Temperature * 9) / 5 + 32);
+  const changeTemerature = ({ newvalue, iscelsius }) => {
+    setTemperature(
+      iscelsius ? (newvalue * 9) / 5 + 32 : ((newvalue - 32) * 5) / 9
+    );
   };
-  // const changecelsius = (Farenheit) => {
-  //   console.log(Farenheit);
-  //   setfarenheit(Farenheit);
-  //   setTemperature(((Farenheit - 32) * 5) / 9);
-  //   console.log(Farenheit);
-  // };
 
   return (
     <div className="bg-slate-800 text-white h-screen ">
@@ -34,8 +32,7 @@ function App({ label, onChange, value, name }) {
             <Page
               label={data.label}
               value={Temperature}
-              name={data.name}
-              onChange={changeTemerature}
+              onChange={data.onChange}
             />
           ))}
         </div>
